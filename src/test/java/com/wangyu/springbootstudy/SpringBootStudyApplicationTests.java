8d3fe254f8f132d100c05f83bc701abe69ad641f
wangyubj15 <wangyubj15@kanyun.com>
2023-10-12 18:14:08 +0800

    init commit

---
+++ src/test/java/com/wangyu/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangyu.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
